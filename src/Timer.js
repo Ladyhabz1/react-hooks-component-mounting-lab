c638efe5475d8d4cc4179ca45fd55db4679a2c1d
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-08 13:51:36 +0300

    complete labs

--- src/Timer.js
+++ src/Timer.js
@@ -1,40 +1,32 @@
 import React, { Component } from "react";
 
 class Timer extends Component {
-  state = {
-    time: 0,
-    color: "#" + Math.floor(Math.random() * 16777215).toString(16)
-  };
-
-  // add your code here
-
-  render() {
-    const { time, color } = this.state;
-    return (
-      <section className="Timer" style={{ background: color }}>
-        <h1>{time}</h1>
-        <button onClick={this.stopClock}>Stop</button>
-        <aside className="mountText">Mounted</aside>
-        <small onClick={this.handleClose}>X</small>
-      </section>
-    );
+  constructor() {
+    super();
+    this.state = {
+      time: 0,
+    };
   }
 
-  //clock functions
   clockTick = () => {
-    this.setState(prevState => ({
-      time: prevState.time + 1
+    this.setState((prevState) => ({
+      time: prevState.time + 1,
     }));
   };
 
-  stopClock = () => {
-    clearInterval(this.interval);
-  };
+  // ✅ Start the timer when the component mounts
+  componentDidMount() {
+    this.interval = setInterval(() => this.clockTick(), 1000);
+  }
 
-  // for the 'x' button,
-  handleClose = () => {
-    this.props.removeTimer(this.props.id);
-  };
+  // ✅ Stop the timer when the component unmounts
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <div>Time: {this.state.time} seconds</div>;
+  }
 }
 
 export default Timer;
